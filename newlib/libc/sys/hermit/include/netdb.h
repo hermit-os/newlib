8623a11ab0b4283cd59799df56fd0ae37caa994d
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-08-30 16:52:49 +0200

    fix(netdb.h): `sockaddr_in` definition
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- newlib/libc/sys/hermit/include/netdb.h
+++ newlib/libc/sys/hermit/include/netdb.h
@@ -25,10 +25,11 @@ struct in_addr {
 };
 
 struct sockaddr_in {
-    short            sin_family;   // e.g. AF_INET
-    unsigned short   sin_port;     // e.g. htons(3490)
-    struct in_addr   sin_addr;     // see struct in_addr, below
-    char             sin_zero[8];  // zero this if you want to
+    uint8_t sin_len;
+    uint8_t sin_family;
+    uint16_t sin_port;
+    struct in_addr sin_addr;
+    char sin_zero[8];
 };
 
 #ifdef __cplusplus
