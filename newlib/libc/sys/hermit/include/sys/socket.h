fd7ae2927c98664120d140017b059d24c877a84c
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-12-03 17:29:01 +0100

    style(socket.h): don't wrap function definitions

--- newlib/libc/sys/hermit/include/sys/socket.h
+++ newlib/libc/sys/hermit/include/sys/socket.h
@@ -49,33 +49,16 @@ int32_t sys_listen(int32_t s, int32_t backlog);
 int32_t sys_bind(int32_t s, const struct sockaddr *name, socklen_t namelen);
 int32_t sys_connect(int32_t s, const struct sockaddr *name, socklen_t namelen);
 int32_t sys_getsockname(int32_t s, struct sockaddr *name, socklen_t *namelen);
-int32_t sys_setsockopt(int32_t s,
-                       int32_t level,
-                       int32_t optname,
-                       const void *optval,
-                       socklen_t optlen);
+int32_t sys_setsockopt(int32_t s, int32_t level, int32_t optname, const void *optval, socklen_t optlen);
 int32_t sys_getsockopt(int32_t s, int32_t level, int32_t optname, void *optval, socklen_t *optlen);
 int32_t sys_getpeername(int32_t s, struct sockaddr *name, socklen_t *namelen);
 void sys_freeaddrinfo(addrinfo *ai);
-int32_t sys_getaddrinfo(const uint8_t *nodename,
-                        const uint8_t *servname,
-                        const addrinfo *hints,
-                        addrinfo **res);
+int32_t sys_getaddrinfo(const uint8_t *nodename, const uint8_t *servname, const addrinfo *hints, addrinfo **res);
 intptr_t sys_send(int32_t s, const void *mem, uintptr_t len, int32_t _flags);
 int32_t sys_shutdown(int32_t s, int32_t how);
 intptr_t sys_recv(int32_t fd, uint8_t *buf, uintptr_t len, int32_t flags);
-intptr_t sys_sendto(int32_t socket,
-                    const uint8_t *buf,
-                    uintptr_t len,
-                    int32_t flags,
-                    const struct sockaddr *addr,
-                    socklen_t addrlen);
-intptr_t sys_recvfrom(int32_t socket,
-                      uint8_t *buf,
-                      uintptr_t len,
-                      int32_t flags,
-                      struct sockaddr *addr,
-                      socklen_t *addrlen);
+intptr_t sys_sendto(int32_t socket, const uint8_t *buf, uintptr_t len, int32_t flags, const struct sockaddr *addr, socklen_t addrlen);
+intptr_t sys_recvfrom(int32_t socket, uint8_t *buf, uintptr_t len, int32_t flags, struct sockaddr *addr, socklen_t *addrlen);
 
 // Defines from librs
 #define AF_INET 0
