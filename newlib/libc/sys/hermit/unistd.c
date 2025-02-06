d3535534837cd9c0c6c542358dc4b0bd5c1ccc72
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-02-06 16:06:47 +0100

    style: reorder `unistd.c`

--- newlib/libc/sys/hermit/unistd.c
+++ newlib/libc/sys/hermit/unistd.c
@@ -21,24 +21,16 @@ uid_t getuid(void) {
 	return 0;
 }
 
-uid_t geteuid(void) {
-	return 0;
-}
-
-gid_t getgid(void) {
-	return 0;
-}
-
-gid_t getegid(void) {
-	return 0;
-}
-
 int setuid(uid_t uid) {
 	if (uid != 0) {
 		errno = EINVAL;
 		return -1;
 	}
 
+	return 0;
+}
+
+uid_t geteuid(void) {
 	return 0;
 }
 
@@ -51,12 +43,20 @@ int seteuid(uid_t euid) {
 	return 0;
 }
 
+gid_t getgid(void) {
+	return 0;
+}
+
 int setgid(gid_t gid) {
 	if (gid != 0) {
 		errno = EINVAL;
 		return -1;
 	}
 
+	return 0;
+}
+
+gid_t getegid(void) {
 	return 0;
 }
 
