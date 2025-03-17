9252593661de0c633bd830cf5357dd3d4810e021
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-03-17 19:51:19 +0100

    feat(signal.h): implement `pthread_sigmask`

--- newlib/libc/sys/hermit/signal.c
+++ newlib/libc/sys/hermit/signal.c
@@ -1,5 +1,14 @@
 #include <signal.h>
 
+int pthread_sigmask(int how, const sigset_t *restrict set,
+                    sigset_t *restrict oset) {
+    if (oset != NULL) {
+        *oset = (sigset_t)-1;
+    }
+
+    return 0;
+}
+
 int sigaction(int sig, const struct sigaction *restrict act,
               struct sigaction *restrict oact) {
     if (oact != NULL) {
