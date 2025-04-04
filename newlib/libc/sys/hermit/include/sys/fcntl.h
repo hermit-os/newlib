cf8e560f13b468487ba2444aafe96734c9d5e81b
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-04-04 16:09:41 +0200

    feat(fcntl.h): add `O_CLOEXEC`

--- newlib/libc/sys/hermit/include/sys/fcntl.h
+++ newlib/libc/sys/hermit/include/sys/fcntl.h
@@ -16,16 +16,17 @@
 
 int fcntl(int fildes, int cmd, ...);
 
-#define O_RDONLY    0000000
-#define O_WRONLY    0000001
-#define O_RDWR      0000002
-#define O_CREAT     0000100
-#define O_EXCL      0000200
-#define O_TRUNC     0001000
-#define O_APPEND    0002000
-#define O_NONBLOCK  0004000
-#define O_DIRECT    0040000
-#define O_DIRECTORY 0200000
+#define O_RDONLY    00000000
+#define O_WRONLY    00000001
+#define O_RDWR      00000002
+#define O_CREAT     00000100
+#define O_EXCL      00000200
+#define O_TRUNC     00001000
+#define O_APPEND    00002000
+#define O_NONBLOCK  00004000
+#define O_DIRECT    00040000
+#define O_DIRECTORY 00200000
+#define O_CLOEXEC   02000000
 
 #define O_ACCMODE (O_RDONLY|O_WRONLY|O_RDWR)
 
