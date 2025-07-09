5d41b7ef91682c8a2a9ca1b03d1dda016e25c141
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-07-09 14:46:37 +0200

    fix(select): handle FD sets being NULL

--- newlib/libc/sys/hermit/select.c
+++ newlib/libc/sys/hermit/select.c
@@ -20,15 +20,15 @@ int select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds,
     for (int fd = 0; fd < nfds; ++fd) {
         struct pollfd poll_spec = {.fd = fd, .events = 0};
 
-        if (FD_ISSET(fd, readfds)) {
+        if (readfds != NULL && FD_ISSET(fd, readfds)) {
             poll_spec.events |= POLLIN;
         }
 
-        if (FD_ISSET(fd, writefds)) {
+        if (writefds != NULL && FD_ISSET(fd, writefds)) {
             poll_spec.events |= POLLOUT;
         }
 
-        if (FD_ISSET(fd, errorfds)) {
+        if (errorfds != NULL && FD_ISSET(fd, errorfds)) {
             poll_spec.events |= POLLERR;
         }
 
@@ -49,21 +49,30 @@ int select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds,
         return ret;
     }
 
-    FD_ZERO(readfds);
-    FD_ZERO(writefds);
-    FD_ZERO(errorfds);
+    if (readfds != NULL) {
+        FD_ZERO(readfds);
+    }
+
+    if (writefds != NULL) {
+        FD_ZERO(writefds);
+    }
+
+    if (errorfds != NULL) {
+        FD_ZERO(errorfds);
+    }
+
     for (int i = 0; i < poll_nfds; ++i) {
         struct pollfd poll_spec = pollfds[i];
 
-        if ((poll_spec.revents & POLLIN) == POLLIN) {
+        if (readfds != NULL && (poll_spec.revents & POLLIN) == POLLIN) {
             FD_SET(poll_spec.fd, readfds);
         }
 
-        if ((poll_spec.revents & POLLOUT) == POLLOUT) {
+        if (writefds != NULL && (poll_spec.revents & POLLOUT) == POLLOUT) {
             FD_SET(poll_spec.fd, writefds);
         }
 
-        if ((poll_spec.revents & POLLERR) == POLLERR) {
+        if (errorfds != NULL && (poll_spec.revents & POLLERR) == POLLERR) {
             FD_SET(poll_spec.fd, errorfds);
         }
     }
